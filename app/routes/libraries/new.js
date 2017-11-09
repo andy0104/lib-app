97704a2d0512f155a559863489f2d07f6240494f
Aninda Kar <aninda.kar@outlook.com>
2017-11-09 19:57:42 +0530

    libraries

--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model(){
+        return this.store.createRecord('library');
+    },
+    actions: {
+        saveLibrary(newLibrary){
+            newLibrary.save().then(() => {
+                this.transitionTo('libraries');
+            })
+        },
+        willTransition(){
+            //will rollback data from table
+            //if the model is isNew
+            this.controller.get('model').rollbackAttributes();
+        }
+    }
 });
